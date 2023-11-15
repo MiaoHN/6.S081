5b955cbd870abaaa6a9ef20d346442c7829e5296
MiaoHN <582418227@qq.com>
2023-11-15 16:20:35 +0800

    finish find

---
+++ user/find.c
@@ -0,0 +1,63 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+#include "kernel/fs.h"
+
+void
+find(char* dir, char *name)
+{
+  char buf[512], *p;
+  int fd;
+  struct dirent de;
+  struct stat st;
+
+  if ((fd = open(dir, 0)) < 0) {
+    fprintf(2, "find: cannot open %s\n", dir);
+    return;
+  }
+
+  if (fstat(fd, &st) < 0) {
+    fprintf(2, "find: cannot stat %s\n", dir);
+    close(fd);
+    return;
+  }
+  if (st.type != T_DIR) {
+    close(fd);
+    return;
+  }
+  if (strlen(dir) + 1 + DIRSIZ + 1 > sizeof buf) {
+    printf("find: path too long\n");
+    close(fd);
+    return;
+  }
+  strcpy(buf, dir);
+  p = buf + strlen(buf);
+  *p++ = '/';
+
+  while (read(fd, &de, sizeof(de)) == sizeof(de)) {
+    if (de.inum == 0)
+      continue;
+    memmove(p, de.name, DIRSIZ);
+    p[DIRSIZ] = 0;
+    if (strcmp(p, ".") == 0 || strcmp(p, "..") == 0)
+      continue;
+    if (strcmp(p, name) == 0) {
+      printf("%s\n", buf);
+    }
+    find(buf, name);
+  }
+  close(fd);
+}
+
+int
+main(int argc, char *argv[])
+{
+  if (argc != 3) {
+    printf("usage: find dir name\n");
+    exit(1);
+  }
+
+  find(argv[1], argv[2]);
+
+  exit(0);
+}
